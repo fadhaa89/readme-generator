37b9366662d03f569fe6bfded0d3f977b69be406
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-04 13:42:18 -0500

    more questions are added

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+
 // TODO: Include packages needed for this application
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 // const MARKDOWN = require("./utils/generateMarkdown");
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // TODO: Create an array of questions for user input
 const questions = [
     {
       
     type: "input",
-    name: "title",
+    name: "Title",
     message: "What is your project's name?",  
     },
     {
         type: "input",
-        name: "email",
+        name: "Email",
         message: "What is your email address?",
     },
+    {
+        type: "input",
+        name: "Description",
+        message: "write a brief description of your project",
+    },
+    {
+        type: "input",
+        name: "Acknowledgements",
+        message: "Any acknowledgements to include? "
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "any Installation instructions ?",  
+     },
+     {
+        type: "input",
+        name: "Usage",
+        message: "What is this App designed to do ?",
+    },
+
+        
+
 ];
 const userInput = {};
 
