abe380c648394ab0c90a8506ab337c2dce1447e9
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-04 14:19:24 -0500

    all questions prompt now working

--- index.js
+++ index.js
@@ -7,64 +7,69 @@ const fs = require("fs");
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      
+    {  
     type: "input",
     name: "Title",
     message: "What is your project's name?",  
     },
+
     {
         type: "input",
         name: "Email",
         message: "What is your email address?",
     },
+
     {
         type: "input",
         name: "Description",
         message: "write a brief description of your project",
     },
+
     {
         type: "input",
         name: "Acknowledgements",
         message: "Any acknowledgements to include? "
     },
+
     {
         type: "input",
         name: "Installation",
         message: "any Installation instructions ?",  
      },
+
      {
         type: "input",
         name: "Usage",
         message: "What is this App designed to do ?",
     },
+
     {
         type: "input",
         name: "Repo",
         message: "What is your Repo link ?",
     },
+
     {
         type: "input",
         name: "Githubname",
         message: "What is your Github name ?",
     },
+
     {
         type: "input",
         name: "Githublink",
         message: "What is your Github link ?",
     },
 
-
-
-        
-
 ];
-const userInput = {};
+
+let userInput = {};
 
 function getUserInformation() {
     return inquirer.prompt(questions).then(function (answers) {
         console.log("answers: ", answers);
         userInput = answers;
+
         // userData.title = userInput.title,
         // userData.introText = userInput.introText,
         // userData.instructions = userInput.instructions,
@@ -81,14 +86,21 @@ function getUserInformation() {
         // dd installation, license, tests, questions
         console.log("userInput: ", userInput);
     })
-};
+}
+
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
+// function init () {
+//     getUserInformation();
+// }
+
 // TODO: Create a function to initialize app
-function init() {
-    getUserInformation()
+async function init() {
+    await getUserInformation();
+    // writeFile(userData);
+    // writeFile();
 }
 
 // Function call to initialize app
-init();
+init()
